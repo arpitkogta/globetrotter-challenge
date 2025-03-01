4cddf797ea6afc1488dc2faa4964ab21a1b09627
Arpit <arpit@Apoorvas-MacBook-Air.local>
2025-03-01 18:52:50 +0530

    added-data-initializer

--- globetrotter-be/src/main/java/com/globetrotter/GlobetrotterApplication.java
+++ globetrotter-be/src/main/java/com/globetrotter/GlobetrotterApplication.java
@@ -2,8 +2,11 @@ package com.globetrotter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
+@EntityScan("com.globetrotter.model")
 public class GlobetrotterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GlobetrotterApplication.class, args);
